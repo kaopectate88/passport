f75b5214d1b71a65b425be3d2ce17310f701b789
kao2 <anthonypauljohnson@gmail.com>
2017-11-06 20:33:44 -0600

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://kaopectate:kaopectate@ds149905.mlab.com:49905/userlogins' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
